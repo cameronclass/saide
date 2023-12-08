0e375d90f6c72582f6535695e60fa8c234862598
cameronclass <kamrankutumov@gmail.com>
2023-12-08 17:13:28 +0300

    initial commit

--- src/assets/js/common/main.js
+++ src/assets/js/common/main.js
@@ -18,4 +18,32 @@ document.addEventListener("DOMContentLoaded", () => {
       this.classList.toggle("menu-item__active");
     });
   });
+
+  // Получаем все элементы с классом .menu-item__card
+  const menuItemsInside = document.querySelectorAll(
+    ".menu-item__cards>.menu-item__card"
+  );
+
+  // Функция, которая будет добавлять класс active и скрывать другие элементы
+  function toggleActiveClass() {
+    // Добавляем класс active текущему элементу
+    this.classList.toggle("menu-item__card_active");
+
+    // Проверяем, есть ли у текущего элемента класс active
+    var isActive = this.classList.contains("menu-item__card_active");
+
+    // Скрываем/показываем остальные элементы в зависимости от наличия класса active
+    menuItemsInside.forEach(function (menuItem) {
+      if (menuItem !== this) {
+        menuItem.classList.toggle("d-none", isActive);
+      }
+    }, this);
+  }
+
+  // Назначаем обработчик события click для каждого элемента меню
+  menuItemsInside.forEach(function (menuItem) {
+    menuItem.addEventListener("click", toggleActiveClass);
+  });
+
+  toggleActiveClass();
 });
